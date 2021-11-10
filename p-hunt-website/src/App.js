fb847296c2e6ef44d36c3c15b003c4fc464199c2
harsh <harshchaturvedi589@gmail.com>
2021-11-10 11:24:30 +0530

    community page setup

--- p-hunt-website/src/App.js
+++ p-hunt-website/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+// temporary code for navbar
+import { useEffect, useState } from "react"
+
+import { Community } from './communityPage/communityPage';
+
 function App() {
+  // temporary code for navbar
+  const [offset, setOffset] = useState(0);
+  useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset)
+    }
+  }, []);
+  console.log(offset);
+  // -----------------------------------
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* temporary div for navbar */}
+      <div className="one" style={{ position: offset > 10 ? "fixed" : "", marginTop: offset > 10 ? "0px" : "10px" }}>Navbar code</div>
+      {/* -------------------- */}
+      <Community />
     </div>
   );
 }
